dd35c92ee9a6300e756aca0cd6d494777378ced7
RaphaelNY <1278173058@qq.com>
2023-10-25 11:05:50 +0800

    task_1_1 correct

--- task_1_1.py
+++ task_1_1.py
@@ -1,18 +1,18 @@
 temperature_input = input("请输入带有符号的温度值：")
 
 if temperature_input.endswith('C') or temperature_input.endswith('c'):
-        temperature_celsius = float(temperature_input[:-1])
+        temp_c = float(temperature_input[:-1])
 
-        temperature_fahrenheit = 1.8 * temperature_celsius + 32
+        temp_f = 1.8 * temp_c + 32
 
-        print("转换后的温度是{:.2f}F".format(temperature_fahrenheit))
+        print("转换后的温度是{:.2f}F".format(temp_f))
 
 elif temperature_input.endswith('F') or temperature_input.endswith('f'):
-        temperature_fahrenheit = float(temperature_input[:-1])
+        temp_f = float(temperature_input[:-1])
 
-        temperature_celsius = (temperature_fahrenheit - 32) / 1.8
+        temp_c = (temp_f - 32) / 1.8
 
-        print("转换后的温度是{:.2f}C".format(temperature_celsius))
+        print("转换后的温度是{:.2f}C".format(temp_c))
 
 else:
     print("输入格式错误")
