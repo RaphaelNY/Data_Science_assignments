d0dbb7978db4280f671f3a71f4eb12a197021121
RaphaelNY <1278173058@qq.com>
2023-11-22 11:01:23 +0800

    task 2

---
+++ lab2/task_2.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+pop = np.random.normal(1, 50, 2000)
+
+means_size_100 = [np.mean(np.random.choice(pop, size=100)) for _ in range(10000)]
+
+plt.hist(means_size_100, bins=30, color='g')
+plt.grid()
+plt.show()
