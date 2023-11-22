2eb8346d2a1e6ab483339e1fc374672642843de8
RaphaelNY <1278173058@qq.com>
2023-11-22 11:08:30 +0800

    task 3

--- lab2/task_3.py
+++ lab2/task_3.py
@@ -10,9 +10,9 @@ n = 0
 
 for i in range(times):
     bootstrapSamples = resample(list_all, n_samples=100, replace=1)
-    n += np.sum(bootstrapSamples == 1)/100
+    n += np.sum(bootstrapSamples == 1)
 
-average = n / times * 100
+average = (n / 100) / times * 100
 
 print("calculate average is ", average, "%")
 print("raw data average is ", 1000/1800*100, "%")
