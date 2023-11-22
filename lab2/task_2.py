16975633a1fe64cc73d12b0fbaff714b050f688a
RaphaelNY <1278173058@qq.com>
2023-11-22 11:21:04 +0800

    task 2

--- lab2/task_2.py
+++ lab2/task_2.py
@@ -6,5 +6,7 @@ pop = np.random.normal(1, 50, 2000)
 means_size_100 = [np.mean(np.random.choice(pop, size=100)) for _ in range(10000)]
 
 plt.hist(means_size_100, bins=30, color='g')
+plt.xlabel('Value')
+plt.ylabel('Frequency')
 plt.grid()
 plt.show()
